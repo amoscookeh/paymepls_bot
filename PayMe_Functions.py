33b0698196e45d31d851a42d864a6fb9b43a2474
AmosCookeh <amosliew1999@gmail.com>
2021-01-06 02:49:54 +0800

    errorlogger

--- PayMe_Functions.py
+++ PayMe_Functions.py
@@ -10,6 +10,7 @@ TOKEN = os.environ['TOKEN']
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # First time starting the bot
 def start (update, context):
@@ -284,6 +285,9 @@ def paid(update, context, name, poll_id):
 #     summary = "Summary of payment: " + str(title) + "\n\nPAID: " + str(paid) + "\n\nUNPAID: " + str(unpaid)
 #     return summary
 
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 # First Time Login Conversation
 USERNAME, PAYMENT_METHOD, LINK, READY = range(4)
 PUBLISH, DELETE = range(2)
@@ -346,6 +350,7 @@ def main():
 	dispatcher.add_handler(conv_handler)
 	dispatcher.add_handler(InlineQueryHandler(inlinequery))
 	dispatcher.add_handler(CallbackQueryHandler(callbackhandle))
+	dispatcher.add_error_handler(error)
 
 	updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
