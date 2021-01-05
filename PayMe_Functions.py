a59a19c602d48158713d7f6086eabeae0dec0526
AmosCookeh <amosliew1999@gmail.com>
2021-01-06 02:29:13 +0800

    commit15

--- PayMe_Functions.py
+++ PayMe_Functions.py
@@ -355,7 +355,7 @@ def main():
 	updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-	updater.bot.setWebhook('https://git.heroku.com/paymeplsbot.git/' + TOKEN)
+	updater.bot.setWebhook('https://paymeplsbot.herokuapp.com/ ' + TOKEN)
 
 	updater.idle()
 
